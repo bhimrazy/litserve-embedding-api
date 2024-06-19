d60e7e6f7ae3610b2a83b02a4d81b04a978918c0
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-06-19 19:19:35 +0000

    adds initial files

---
+++ server.py
@@ -0,0 +1,27 @@
+# server.py
+import litserve as ls
+
+# STEP 1: DEFINE YOUR MODEL API
+class SimpleLitAPI(ls.LitAPI):
+    def setup(self, device):
+        # Setup the model so it can be called in `predict`.
+        self.model = lambda x: x**2
+
+    def decode_request(self, request):
+        # Convert the request payload to your model input.
+        return request["input"]
+
+    def predict(self, x):
+        # Run the model on the input and return the output.
+        return self.model(x)
+
+    def encode_response(self, output):
+        # Convert the model output to a response payload.
+        return {"output": output}
+
+
+# STEP 2: START THE SERVER
+if __name__ == "__main__":
+    api = SimpleLitAPI()
+    server = ls.LitServer(api, accelerator="auto")
+    server.run(port=8000)
