ae55767467c9b96f51b2f256ffb3093c647d1978
bhimrazy <bhimrajyadav977@gmail.com>
2024-06-20 14:15:40 +0545

    feat: add client code for making API requests
    This commit adds the client code for making API requests to the OpenAI service. It includes an example of how to use the client to create embeddings using the "jina-embeddings-v2-small-en" model. The code also includes a commented out cURL command for making the same request.

---
+++ client.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+client = OpenAI(base_url="http://127.0.0.1:8000/v1/", api_key="lit")
+
+response = client.embeddings.create(
+    model="jina-embeddings-v2-small-en",
+    input="The food was delicious and the waiter...",
+    encoding_format="float",
+)
+
+print(response)
+
+"""
+curl http://127.0.0.1:8000/v1/embeddings \
+  -H "Content-Type: application/json" \
+  -d '{
+    "input": "The food was delicious and the waiter...",
+    "model": "jina-embeddings-v2-small-en",
+    "encoding_format": "float"
+  }'
+"""
