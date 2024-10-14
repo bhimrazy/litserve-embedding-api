68610ce8ffe26a97e95a1d36497d040bddb63310
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-10-14 14:48:09 +0000

    updated model name

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ curl http://127.0.0.1:8000/v1/embeddings \
   -H "Content-Type: application/json" \
   -d '{
     "input": "The food was delicious and the waiter...",
-    "model": "jina-embeddings-v2-small-en",
+    "model": "jinaai/jina-embeddings-v2-small-en",
     "encoding_format": "float"
   }'
 """
@@ -13,7 +13,7 @@ from openai import OpenAI
 client = OpenAI(base_url="http://127.0.0.1:8000/v1/", api_key="lit")
 
 response = client.embeddings.create(
-    model="jina-embeddings-v2-small-en",
+    model="jinaai/jina-embeddings-v2-small-en",
     input="The food was delicious and the waiter...",
     encoding_format="float",
 )
